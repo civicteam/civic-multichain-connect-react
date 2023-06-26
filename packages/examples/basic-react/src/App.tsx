2755ef646d4100a1e65ba3469391737773db15da
Daneel Smuts <daneelsmuts@gmail.com>
2023-06-26 14:15:56 +0200

    chore: clean up basic sample app

--- packages/examples/basic-react/src/App.tsx
+++ packages/examples/basic-react/src/App.tsx
@@ -1,14 +1,20 @@
 /* eslint-disable require-extensions/require-extensions */
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 import {
   MultichainConnectButton,
   MultichainWalletProvider,
 } from "@civic/multichain-connect-react-core";
 import { clusterApiUrl } from "@solana/web3.js";
-import { SolanaWalletAdapterConfig } from "@civic/multichain-connect-react-solana-wallet-adapter";
+import {
+  SolanaWalletAdapterConfig,
+  Chain as SolanaChain,
+} from "@civic/multichain-connect-react-solana-wallet-adapter";
 import { publicProvider } from "wagmi/providers/public";
-import { RainbowkitConfig } from "@civic/multichain-connect-react-rainbowkit-wallet-adapter";
+import {
+  RainbowkitConfig,
+  Chain as EthereumChain,
+} from "@civic/multichain-connect-react-rainbowkit-wallet-adapter";
 import {
   mainnet,
   goerli,
@@ -36,20 +42,28 @@ const solanaDevnetChain = {
   rpcEndpoint: clusterApiUrl("devnet"),
 };
 
-const defaultSolanaChains = [solanaMainnetChain, solanaDevnetChain];
+const defaultSolanaChains = [solanaMainnetChain];
+const defaultSolanaTestChains = [solanaDevnetChain];
+
+const defaultEvmChains = [polygon, mainnet, arbitrum];
+const defaultEvmTestChains = [polygonMumbai, goerli, arbitrumGoerli];
 
 const Content = () => {
   return (
     <MultichainWalletProvider>
       <RainbowkitConfig
-        chains={clientChains}
+        chains={defaultEvmChains}
+        testnetChains={defaultEvmTestChains}
         providers={[publicProvider()]}
         options={{
           // Rainbowkit relies on WalletConnect which now needs to obtain a projectId from WalletConnect Cloud.
           walletConnectProjectId: "*YOUR WALLET CONNECT PROJECT ID*",
         }}
       >
-        <SolanaWalletAdapterConfig chains={defaultSolanaChains}>
+        <SolanaWalletAdapterConfig
+          chains={defaultSolanaChains}
+          testnetChains={defaultSolanaTestChains}
+        >
           <MultichainConnectButton />
         </SolanaWalletAdapterConfig>
       </RainbowkitConfig>
