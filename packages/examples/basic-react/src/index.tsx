ad07dcf4047c637c54329dccda676f46ac159e49
Pedro Filho <pedro@filho.me>
2024-09-05 11:52:13 -0300

    feat/component library (#61)
    * adding component library to multichain connect react
    
    * adding config for advanced

--- packages/examples/basic-react/src/index.tsx
+++ packages/examples/basic-react/src/index.tsx
@@ -1,7 +1,7 @@
+/* eslint-disable require-extensions/require-extensions */
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-// eslint-disable-next-line require-extensions/require-extensions
 import App from "./App";
 
 const root = ReactDOM.createRoot(
